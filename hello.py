10e3df83a444a2fb3b0b2ac3fc6b8031e823db31
MattDekinder <dekinder@ualberta.ca>
2017-01-24 09:15:07 -0700

    working up to the login page

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python
+import os, json, urlparse
+print 'Content-Type: application/json'
+print '\r\n'
+print urlparse.parse_qs(os.environ['QUERY_STRING'])
+
+user_agent = os.environ['HTTP_USER_AGENT']
+if 'Firefox' in user_agent:
+	print 'Firefox'
+elif 'Chrome' in user_agent:
+	print 'Chrome'
+elif 'Curl' in user_agent:
+	print 'curl'
+else:
+	print user_agent
+
+
+#print json.dumps(dict(os.environ))

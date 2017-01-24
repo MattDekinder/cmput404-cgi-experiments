f3986e14c6c20777bc4b952a49f39d3691025f11
MattDekinder <dekinder@ualberta.ca>
2017-01-24 10:03:55 -0700

    logging in with cookies

--- hello.py
+++ hello.py
@@ -2,29 +2,42 @@
 import os, json, urlparse, sys
 from templates import login_page
 
+logged_in = False
+username = 'matt'
+password = 'asdf'
+content_length = os.environ["CONTENT_LENGTH"]
+cookie = os.environ["HTTP_COOKIE"]
+if cookie =='logged-in=True':
+	logged_in = True
+
+#if this header is here, then the server has sent stuff
+elif content_length:
+	bytes_to_read = int(content_length)
+	content = sys.stdin.read(bytes_to_read)
+
+	params=  urlparse.parse_qs(content)
+	if params['username'][0]==username and params['password'][0]==password:
+		print 'Set-Cookie: logged-in=True'
+		logged_in = True
+
+
 print 'Content-Type: text/html'
 print '\r\n'
 
-print    r"""
-    <h1> Welcome! </h1>
+if not logged_in:
+	print    r"""
+		<h1> Welcome! </h1>
 
-    <form method="POST" action="hello.py">
-        <label> <span>Username:</span> <input autofocus type="text" name="username"></label> <br>
-        <label> <span>Password:</span> <input type="password" name="password"></label>
+		<form method="POST" action="hello.py">
+		    <label> <span>Username:</span> <input autofocus type="text" name="username"></label> <br>
+		    <label> <span>Password:</span> <input type="password" name="password"></label>
 
-        <button type="submit"> Login! </button>
-    </form>
-    """
+		    <button type="submit"> Login! </button>
+		</form>
+		"""
 
-#if this header is here, then the serve has sent stuff
-content_length = os.environ["CONTENT_LENGTH"]
-if content_length:
-	bytes_to_read = int(content_length)
-	content = sys.stdin.read(bytes_to_read)
-	print "<pre>"
-	print content
-	print "</pre>"
-
+if logged_in:
+	print "hello"
 
 
 #print urlparse.parse_qs(os.environ['QUERY_STRING'])
